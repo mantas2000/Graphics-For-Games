c199a5d9fe857b2bbbc65b1e04d903949718046e
Mantas Burcikas <b9037068@newcastle.ac.uk>
2021-11-23 19:21:40 +0000

    Updated RotateAroundPoint controller

--- CSC3231-Graphics-For-Games/Assets/Scripts/RotateAroundPoint.cs
+++ CSC3231-Graphics-For-Games/Assets/Scripts/RotateAroundPoint.cs
@@ -1,16 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateAroundPoint : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private bool rotateY;
+    [SerializeField] private bool rotateZ;
     public GameObject pivotObject;
 
     // Update is called once per frame
     private void Update()
     {
         // Move around selected game object
-        transform.RotateAround(pivotObject.transform.position, new Vector3(0, 1, 0), rotationSpeed * Time.deltaTime);
+        transform.RotateAround(pivotObject.transform.position, new Vector3(0, rotateY ? 1 : 0, rotateZ ? 1 : 0), rotationSpeed * Time.deltaTime);
     }
 }
